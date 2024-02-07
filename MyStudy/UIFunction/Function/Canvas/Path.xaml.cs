a37a8f25d3bd761cb7a14d026124ce291c26a293
JoeyYang <joeyyang@pcpartner.com>
2024-02-07 16:11:55 +0800

    Path: First sample (Canvas)
    Reference: https://www.youtube.com/watch?v=lPZ4R1L0fWU

---
+++ MyStudy/UIFunction/Function/Canvas/Path.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * Author: Joey
+ * Reference: https://www.youtube.com/watch?v=lPZ4R1L0fWU
+ */
+namespace UIFunction.Function.Canvas
+{
+    /// <summary>
+    /// Interaction logic for Path.xaml
+    /// </summary>
+    public partial class Path : UserControl
+    {
+        public Path()
+        {
+            InitializeComponent();
+        }
+    }
+}
