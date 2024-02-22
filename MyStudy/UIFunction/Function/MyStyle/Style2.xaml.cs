d1bc5bf5819056f89090b5729bd5f4dcbfedb740
JoeyYang <joeyyang@pcpartner.com>
2024-02-22 16:33:37 +0800

    Style: Build second sample
    Reference:
    https://learn.microsoft.com/en-us/dotnet/desktop/wpf/controls/how-to-create-apply-template?view=netdesktop-8.0

---
+++ MyStudy/UIFunction/Function/MyStyle/Style2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * Reference: 
+ * 1. https://learn.microsoft.com/zh-tw/dotnet/desktop/wpf/controls/how-to-create-apply-template?view=netdesktop-8.0
+ */
+namespace UIFunction.Function.MyStyle
+{
+    /// <summary>
+    /// Interaction logic for Style2.xaml
+    /// </summary>
+    public partial class Style2 : UserControl
+    {
+        public Style2()
+        {
+            InitializeComponent();
+        }
+    }
+}
