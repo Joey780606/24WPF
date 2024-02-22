526d5e9d7acf33b52436833bd86620782331ebde
JoeyYang <joeyyang@pcpartner.com>
2024-02-22 11:46:41 +0800

    Style: First sample

---
+++ MyStudy/UIFunction/Function/MyStyle/Style1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * Reference:
+ * 1. https://learn.microsoft.com/zh-tw/dotnet/desktop/wpf/controls/styles-templates-overview?view=netdesktop-8.0
+ * 2. https://www.huanlintalk.com/2012/11/wpf-controltemplate-datatemplate.html
+ * 
+ */
+namespace UIFunction.Function.MyStyle
+{
+    /// <summary>
+    /// Interaction logic for Style1.xaml
+    /// </summary>
+    public partial class Style1 : UserControl
+    {
+        public Style1()
+        {
+            InitializeComponent();
+        }
+    }
+}
