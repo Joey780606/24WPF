eadbd4e6d13c63693a3a27c14ec8298706daca68
JoeyYang <joeyyang@pcpartner.com>
2024-05-20 16:51:51 +0800

    Join screen record function
    It still need to join screenshot job.

--- MyStudy/UIFunction/Function/WorkItem/Detail0_5.xaml.cs
+++ MyStudy/UIFunction/Function/WorkItem/Detail0_5.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -41,7 +43,8 @@ namespace UIFunction.Function.WorkItem
 
             foreach (var i in scoreQuery)
             {
-                Console.WriteLine("Result: {0} ", i);
+                //Console.WriteLine("Result: {0} ", i);
+                Debug.WriteLine("Result: {0} ", i);
             }
         }
     }
