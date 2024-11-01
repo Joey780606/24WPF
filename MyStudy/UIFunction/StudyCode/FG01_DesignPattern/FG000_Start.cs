6e0f48435bc6bb8f33561ac8b2bdf9084157b772
JoeyYang <joeyyang@pcpartner.com>
2024-11-01 17:30:30 +0800

    Design pattern list info

---
+++ MyStudy/UIFunction/StudyCode/FG01_DesignPattern/FG000_Start.cs
@@ -0,0 +1,57 @@
+﻿/*
+Issue: Design pattern: 
+  Ref:
+  1. https://www.dofactory.com/net/design-patterns
+  2. 種類
+    Creational Patterns
+    1.1 Abstract Factory
+     Creates an instance of several families of classes
+    1.2 Builder	
+     Separates object construction from its representation
+    1.3 Factory Method
+     Creates an instance of several derived classes
+    1.4 Prototype
+     A fully initialized instance to be copied or cloned
+    1.5 Singleton
+     A class of which only a single instance can exist
+
+    Structural Patterns
+    2.1 Adapter
+     Match interfaces of different classes
+    2.2 Bridge
+     Separates an object’s interface from its implementation
+    2.3 Composite
+     A tree structure of simple and composite objects
+    2.4 Decorator
+     Add responsibilities to objects dynamically
+    2.5 Facade
+     A single class that represents an entire subsystem
+    2.6 Flyweight
+     A fine-grained instance used for efficient sharing
+    2.7 Proxy
+     An object representing another object
+
+    Behavioral Patterns
+    3.1 Chain of Resp.
+     A way of passing a request between a chain of objects
+    3.2 Command
+     Encapsulate a command request as an object
+    3.3 Interpreter
+     A way to include language elements in a program
+    3.4 Iterator
+     Sequentially access the elements of a collection
+    3.5 Mediator
+     Defines simplified communication between classes
+    3.6 Memento
+     Capture and restore an object's internal state
+    3.7 Observer
+     A way of notifying change to a number of classes
+    3.8 State
+     Alter an object's behavior when its state changes
+    3.9 Strategy
+     Encapsulates an algorithm inside a class
+    3.10 Template Method
+     Defer the exact steps of an algorithm to a subclass
+    3.11 Visitor
+     Defines a new operation to a class without change
+*/
