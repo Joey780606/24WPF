0fe44b87501a66a78b12743e7cb50ba571bf5dcf
JoeyYang <joeyyang@pcpartner.com>
2024-11-11 08:15:16 +0800

    Join design pattern

---
+++ MyStudy/UIFunction/StudyCode/FG01_DesignPattern/FG0101_AbstractFactory.cs
@@ -0,0 +1,87 @@
+﻿/*
+1. Function: Abstract factory design patter
+2. Detail: Find some documents and related code.
+3. Keyword:
+4. 
+ */
+
+/*
+1. 中文說明:
+  https://www.youtube.com/watch?v=L4TD6psvGn4
+  說明內容: 
+  1. 情境: 有個辨識產品的Library(有分跑CPU和GPU),如何設計可臨時互換成跑CPU或GPU呢?
+    Joey心得: 像是二層(或多層) interface, 一個interface是為了 Product檢查器
+  2. 程式:
+    //3:13
+    var product = new Product() { Name = "C300" };
+
+    IProductInspectorFactory factory = new CPUProductInspectorFactory();    //如果想換回GPU, 就改成 GPUProductInspectorFactory
+    ProductService productService = new ProductService(factory);
+
+    productService.Inspect(product);    
+
+    class Product   //0:26
+    {
+        public string Name { get; set; }
+    }
+
+    interface IProductInspector //0:33
+    {
+        string Inspector(Product product);
+    }
+
+    class CPUProductInspector: IProductInspector    //0:45
+    {
+        public string Inspector(Product product)
+        {
+            return $"product name: {product.Name}, result by cpu";
+        }
+    }
+
+    class GPUProductInspector: IProductInspector    //1:02
+    {
+        public string Inspector(Product product)
+        {
+            return $"product name: {product.Name}, result by gpu";
+        }
+    }
+
+    interface IProductInspectorFactory //1:20, 建立抽象工廠
+    {
+        IProductInspector CreateProductInspector(); // IProductInspector 是在前面宣告的 interface
+    }
+
+    class CPUProductInspectorFactory: IProductInspectorFactory  //2:28
+    {
+        public IProductInspector CreateProductInspector()
+        {
+            return new CPUProductInspector();   //這是CPU
+        }
+    }
+
+    class GPUProductInspectorFactory: IProductInspectorFactory  //2:47
+    {
+        public IProductInspector CreateProductInspector()
+        {
+            return new GPUProductInspector();   //這是GPU
+        }
+    }
+
+    class ProductService // 1:38 建立給其他工程師使用的模組
+    {
+        private readonly IProductInspectorFactory _productInspectorFactory;
+        // 1:48 右鍵選DI進來,不是很懂
+
+        public ProductService(IProductInspectorFactory productInspectoryFactory)
+        {
+            _productInspectorFactory = productInspectoryFactory;
+        }
+
+        public void Inspect(Product product)   //1:48 辨識的方法
+        {
+            var result = _productInspectorFactory.CreateProductInspector().Inspector(product);
+            Console.WriteLine("save result to db ...");
+            Console.WriteLine(result);
+        }
+    }
+*/
---
+++ MyStudy/UIFunction/StudyCode/FG01_DesignPattern/FG0201_Adapter.cs
@@ -0,0 +1,27 @@
+﻿/*
+1. Function: Adapter design patter
+2. Detail: Find some document and related code.
+3. Keyword:
+4. 
+ */
+
+/*
+1. 中文說明:
+  https://medium.com/andy%E7%9A%84%E8%B6%A3%E5%91%B3%E7%A8%8B%E5%BC%8F%E7%B7%B4%E5%8A%9F%E5%9D%8A/20%E5%80%8B%E4%B8%BB%E9%A1%8C%E5%AD%B8%E6%9C%83%E8%A8%AD%E8%A8%88%E6%A8%A1%E5%BC%8F-%E4%BA%8C-%E8%90%AC%E8%83%BD%E8%BD%89%E6%8E%A5%E5%99%A8adapter%E6%A8%A1%E5%BC%8F-9e73ea5d872a
+  說明內容: 
+  1. Adapter的主要目的是想讓兩種東西可以互相適合，ex:想讓我的電腦投影到大螢幕，必須透過HDMI或VGA的轉接頭才能接上，轉接頭在做的事情就跟Adapter類似，可以將它想像成串接兩個獨立事務的一個媒介。
+  在我們的程式當中，不是所有程式都能直接使用，需要經過一些轉換才可以使用，用來填補“現有的程式”和“新的程式需求”之間差異的設計模式就是Adapter模式，常有人把Adapter稱為Wrapper模式，意即將現有的程式包裝過後讓新的需求也能夠使用。
+  2. Adapter的兩種實現方法: 1.使用繼承(Extend) 2.使用委託(Delegation)
+  3. 有例子,但是Java,找其他C#例子
+*/
+
+/*
+2. 英文範例
+  https://www.youtube.com/watch?v=fJO7SnM74dM
+  a.說明內容: 
+   1. 使用情境: 已有舊的library,要研發新的library,有些功能舊的依然可做到,設計時就可使用此pattern,減少時間浪費
+   2:05 本例使用視訊線的例子 (VGA格式要接DVI的格式)
+  
+  https://www.youtube.com/watch?v=HiFOmujCE8c  (這個不錯,但例子還是太多code了)
+  https://medium.com/codenx/adapter-pattern-in-c-b69a32f53f7d (這是簡單的例子,但感覺實作出來 Paypal 和 Strip 還是二個各自的Adapter,無法整合
+*/
